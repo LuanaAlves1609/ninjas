4ed83ddc4ad32a2c6867f7579adeec3dacd89d28
Luana <pereiraalvesluana67@gmail.com>
2026-05-27 20:42:01 -0300

    conclusão do cadastrar

--- src/main/java/br/com/etec/ninjas/NinjasApplication.java
+++ src/main/java/br/com/etec/ninjas/NinjasApplication.java
@@ -10,6 +10,7 @@ public class NinjasApplication {
 		SpringApplication.run(NinjasApplication.class, args);
 	}
 // para executar no terminal coloque: mvn spring-boot:run
+
 // quando adicionar uma dependencia no pom.xml, precisa executar no terminal o comando de atualização do maven para ser 
 //reconhecida no projeto: mvn clean install -U
 }
