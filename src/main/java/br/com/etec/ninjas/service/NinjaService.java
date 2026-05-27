42463e64711b299c1d4180603fc657c807bf9913
Luana <pereiraalvesluana67@gmail.com>
2026-05-26 22:46:24 -0300

    adicionei a lombok, e foram feitas alterações na model + a criação da service

---
+++ src/main/java/br/com/etec/ninjas/service/NinjaService.java
@@ -0,0 +1,18 @@
+package br.com.etec.ninjas.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.etec.ninjas.model.Ninja;
+import br.com.etec.ninjas.repository.NinjaRepository;
+
+@Service
+public class NinjaService {
+
+    @Autowired
+    private NinjaRepository ninjaRepository;
+
+    public Ninja cadastrarNinja (Ninja ninja){
+        return ninjaRepository.save(ninja); 
+    }
+}
